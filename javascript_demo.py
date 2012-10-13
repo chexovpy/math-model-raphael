7ca1aab0399b8b8200e63d17bd6b33a7524c483f
dmitriy.dmitrienko@outlook.com <lordsad1804>
2012-10-13 20:52:35 +0400

    return to old project in Flask

--- javascript_demo.py
+++ javascript_demo.py
@@ -1,6 +1,8 @@
-#coding=UTF8
+#coding=utf-8
+import json
+from flask import Flask,render_template,request,jsonify
 from math import fsum,fabs,pow,sqrt
-__author__ = 'chexov'
+app = Flask(__name__)
 
 def calculate_x_y(x,y):
     """
@@ -41,7 +43,7 @@ def calculate_b_first(n,x,y):
     sum_xy,pow_x,pow_y = calculate_x_y(x,y)
     b = (n*sum_xy - fsum(x)*fsum(y))/(
         n*pow_x - pow(fsum(x),2)
-        )
+    )
     return b
 def calculate_b_zero(n,x,y):
     """
@@ -55,3 +57,27 @@ def calculate_b_zero(n,x,y):
     b_first = calculate_b_first(n,x,y)
     b = fsum(y)/n - b_first*(fsum(x)/n)
     return b
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/graph.json',methods=['POST'])
+def graph_json():
+    if request.method == 'POST':
+        f = request.form
+        x,y,n = json.loads(f['x']),json.loads(f['y']),json.loads(f['n'])
+        rec = correlation_coefficient(n,x,y)
+        func = "y = %s + %s * x"%(calculate_b_zero(n,x,y),calculate_b_first(n,x,y))
+        return jsonify(
+            rec     =   rec,
+            func    =   func,
+            b0      =   calculate_b_zero(n,x,y),
+            b1      =   calculate_b_first(n,x,y),
+            result  =   'ok'
+        )
+    return jsonify(result = 'fail')
+
+if __name__ == '__main__':
+    app.run(host='localhost',debug=True)
+
